600d1871e730e839e1f6d872ca7ab8f5427eadb5
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-09-25 14:01:16 +0300

    node__moduls в gitignore

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import './js/start-loading';
 import './js/countrySearch';
 import './js/scroll-top-btn';
 import './js/team';
+
+import './js/newModalOpen'
+// import './js/newModalRender'
 // import './js/modal-open';
 // import './js/modalRender';
 // import './js/modal-infa';
+
 import './sass/main.scss';
 // import './js/modalFromApi main';
 import './js/preloader';
---
+++ src/js/newModalRender.js
@@ -0,0 +1,2 @@
+import newModalTemplate from '..templates/newModal'
+import ApiService from './api-service';
